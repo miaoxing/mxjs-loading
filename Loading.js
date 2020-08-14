50f9537889bed5a3f2bc8b87b2907c3844fb3531
twinh <twinhuang@qq.com>
2020-08-14 17:12:33 +0800

    style(eslint): single quotes, double jsx-quotes

--- Loading.js
+++ Loading.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Spinner} from "@mxjs/bootstrap";
+import {Spinner} from '@mxjs/bootstrap';
 
 // NOTE: antd spin 会传入 className=ant-spin-dot，以便居中显示
 export default (props) => <Spinner animation="grow" variant="primary" {...props}/>
