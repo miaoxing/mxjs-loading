b26a60c60ec61cbe34073ec1dbf57b705ca1834c
twinh <twinhuang@qq.com>
2020-08-14 17:23:35 +0800

    style(eslint): semi: always

--- Loading.js
+++ Loading.js
@@ -2,4 +2,4 @@ import React from 'react';
 import {Spinner} from '@mxjs/bootstrap';
 
 // NOTE: antd spin 会传入 className=ant-spin-dot，以便居中显示
-export default (props) => <Spinner animation="grow" variant="primary" {...props}/>
+export default (props) => <Spinner animation="grow" variant="primary" {...props}/>;
