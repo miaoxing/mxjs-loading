7ea042b8b516443fbc1574fe4c4a441e8a515fbd
twinh <twinhuang@qq.com>
2021-04-30 21:57:31 +0800

    test(loading): 初始化测试

---
+++ __tests__/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import {Loading} from '..';
+
+describe('loading', () => {
+  test('Loading', () => {
+    const result = render(<Loading/>);
+    expect(result.container).toMatchSnapshot();
+  });
+});
+
